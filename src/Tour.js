fa563961bdfdf873ed03eaf37d801de05e4bb2ae
Souleymane Cisse <slyamciss@gmail.com>
2021-08-13 10:14:23 -0400

    Added Tour  Component

--- src/Tour.js
+++ src/Tour.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Tour({ id, image, price, info, name }) {
+  const [readMore, setReadMore] = useState(false);
   return (
-    <div>
-      <h2>Single Tour Component</h2>
-    </div>
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? "show less" : "read more"}
+          </button>
+        </p>
+        <button className="delete-btn">not interested</button>
+      </footer>
+    </article>
   );
 }
 

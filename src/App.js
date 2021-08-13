20d53cc993c462b3596f6780d2905754067ce80e
Souleymane Cisse <slyamciss@gmail.com>
2021-08-12 22:53:00 -0400

    App & Loading Component

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+import "./App.css";
+
+const url = "https://course-api.com/react-tours-project";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <div className="App">
+      <h2>Ours Tours</h2>
+    </div>
+  );
+}
+
+export default App;
